eb8075ec35aeb9d1b64d1ad69bce55d5e35bef89
kadirchan <kadircanbozkurt1905@gmail.com>
2024-04-17 02:57:23 +0300

    add tree crud to server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import serveStatic from "serve-static";
 import cors from "cors";
-import Database from "./database.js";
 import mongoose from "mongoose";
 import { Game, TreeModel, User } from "./schemas.js";
 import dotenv from "dotenv";
@@ -12,45 +11,19 @@ import { MerkleTree } from "./lib/merkleTree.js";
 
 dotenv.config();
 
-//@ts-ignore
-mongoose.connect(process.env.MONGO);
-const mongoDb = mongoose.connection;
-mongoDb.on("error", (err) => {
-    console.error("MongoDB connection error:", err);
-});
-
-mongoDb.once("open", function () {
-    console.log("Connected successfully to MongoDB");
-});
-
-const publicKey = PrivateKey.random().toPublicKey();
-await (async () => {
-    const map = new MerkleMap();
-    map.set(Poseidon.hash(publicKey.toFields()), Field(31));
-    console.log("Merkle Root", map.getRoot().toString());
-    console.log("Leaf", map.get(Poseidon.hash(publicKey.toFields())).toString());
-
-    const map2 = new MerkleMap();
-    map2.tree = MerkleTree.fromJSON(map.tree.toJSON());
-    console.log("Merkle Root", map2.getRoot().toString());
-    console.log("Leaf", map2.get(Poseidon.hash(publicKey.toFields())).toString());
-
+export async function saveTreeToMongo(map: MerkleMap, name: string) {
     const treeJson = map.tree.toJSON() as {
         height: number;
         nodes: { [key: string]: string };
         name?: string;
     };
-    treeJson.name = "merkleMap";
+    treeJson.name = name;
 
-    await TreeModel.replaceOne(
-        { name: "merkleMap" }, // Filter
-        treeJson, // Replacement document
-        { upsert: true } // Options
-    );
-})();
+    await TreeModel.replaceOne({ name: name }, treeJson, { upsert: true });
+}
 
-await (async () => {
-    const tree = await TreeModel.findOne({ name: "merkleMap" });
+export async function getTreeFromMongo(name: string): Promise<MerkleMap | null> {
+    const tree = await TreeModel.findOne({ name: name });
     if (tree) {
         const treeObject = tree.toObject();
         // @ts-ignore
@@ -63,15 +36,28 @@ await (async () => {
         const map = new MerkleMap();
         map.tree = MerkleTree.fromJSON(treeJson);
 
-        console.log("Merkle Root", map.getRoot().toString());
-        console.log("Leaf", map.get(Poseidon.hash(publicKey.toFields())).toString());
+        return map;
     } else {
-        console.log("No tree found with that name");
+        console.log("No tree found with that name: ", name);
+        return null;
     }
-})();
+}
+
+//@ts-ignore
+mongoose.connect(process.env.MONGO);
+const mongoDb = mongoose.connection;
+mongoDb.on("error", (err) => {
+    console.error("MongoDB connection error:", err);
+});
+
+mongoDb.once("open", function () {
+    console.log("Connected successfully to MongoDB");
+});
+
+const deviceTree = await getTreeFromMongo("deviceTree");
+const sessionTree = await getTreeFromMongo("sessionTree");
 
 const app = express();
-const db = new Database();
 const port = 8080;
 
 app.use(cors());
@@ -128,6 +114,68 @@ app.get("/wishlist/:publicKey", async (req, res) => {
         console.error(err);
         res.status(502).send({ message: "Error retrieving wishlist" });
     }
+});
+
+app.get("/device-tree-leaf/:key", async (req, res) => {
+    const { key } = req.params;
+    const leaf = deviceTree?.get(Field(key));
+    if (leaf) {
+        console.log("Device Tree Leaf Sended.");
+        res.json(leaf);
+    } else {
+        res.status(404).send({ message: "Leaf not found" });
+    }
+});
+
+app.get("/session-tree-leaf/:key", async (req, res) => {
+    const { key } = req.params;
+    const leaf = sessionTree?.get(Field(key));
+    if (leaf) {
+        console.log("Session Tree Leaf Sended.");
+        res.json(leaf);
+    } else {
+        res.status(404).send({ message: "Leaf not found" });
+    }
+});
+
+app.get("/device-tree-witness/:key", async (req, res) => {
+    const { key } = req.params;
+    const witness = deviceTree?.getWitness(Field(key));
+    if (witness) {
+        console.log("Device Tree Witness Sended.");
+        res.json(witness);
+    } else {
+        res.status(404).send({ message: "Witness not found" });
+    }
+});
+
+app.get("/session-tree-witness/:key", async (req, res) => {
+    const { key } = req.params;
+    const witness = sessionTree?.getWitness(Field(key));
+    if (witness) {
+        console.log("Session Tree Witness Sended.");
+        res.json(witness);
+    } else {
+        res.status(404).send({ message: "Witness not found" });
+    }
+});
+
+app.post("/device-tree-leaf", async (req, res) => {
+    const { key, value } = req.body;
+    deviceTree?.set(Field(key), Field(value));
+    // @ts-ignore
+    await saveTreeToMongo(deviceTree, "deviceTree");
+    console.log("Device Tree Leaf Set.");
+    res.status(201).send({ message: "Leaf set" });
+});
+
+app.post("/session-tree-leaf", async (req, res) => {
+    const { key, value } = req.body;
+    sessionTree?.set(Field(key), Field(value));
+    // @ts-ignore
+    await saveTreeToMongo(sessionTree, "sessionTree");
+    console.log("Session Tree Leaf Set.");
+    res.status(201).send({ message: "Leaf set" });
 });
 
 app.listen(port, () => {
